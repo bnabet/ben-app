4e73049585cb01ed69baaeab5d9d803e13b833aa
Benjamin Nabet <bnabet@dydu.ai>
2019-11-07 11:58:50 +0100

    Selecting state slices

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ScrollView, StyleSheet, Image, View, Text, Button } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
-import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
 
@@ -15,8 +15,9 @@ const ListItem = props => {
 };
 
 const MealDetailsScreen = props => {
+	const availableMeals = useSelector(state => state.meals.meals);
 	const mealId = props.navigation.getParam('mealId');
-	const selectedMeal = MEALS.find(meal => meal.id === mealId);
+	const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
 	return (
 		<ScrollView>
