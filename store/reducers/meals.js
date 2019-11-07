94b15f81aade39de0b54a26786b525f4a3e63d82
Benjamin Nabet <ben.nabet08@gmail.com>
2019-11-08 00:47:23 +0100

    Adding filtering logic

--- store/reducers/meals.js
+++ store/reducers/meals.js
@@ -1,10 +1,10 @@
 import { MEALS } from '../../data/dummy-data';
-import { TOGGLE_FAVORITE } from '../actions/meals';
+import { TOGGLE_FAVORITE, SET_FILTERS } from '../actions/meals';
 
 const initialState = {
     meals: MEALS,
     filteredMeals: MEALS,
-    favoriteMeals:  []
+    favoriteMeals: []
 };
 
 const mealsReducer = (state = initialState, action) => {
@@ -19,6 +19,24 @@ const mealsReducer = (state = initialState, action) => {
                 const meal = state.meals.find(meal => meal.id === action.mealId);
                 return { ...state, favoriteMeals: state.favoriteMeals.concat(meal) }
             }
+        case SET_FILTERS:
+            const appliedFilters = action.filters;
+            const updatedFilteredMeals = state.meals.filter(meal => {
+                if (appliedFilters.glutenFree && !meal.isGlutenFree) {
+                    return false;
+                }
+                if (appliedFilters.lactoseFree && !meal.isLactoseFree) {
+                    return false;
+                }
+                if (appliedFilters.vegan && !meal.isVegan) {
+                    return false;
+                }
+                if (appliedFilters.vegetarian && !meal.isVegetarian) {
+                    return false;
+                }
+                return true;
+            });
+            return { ...state, filteredMeals: updatedFilteredMeals }
         default:
             return state;
     }
